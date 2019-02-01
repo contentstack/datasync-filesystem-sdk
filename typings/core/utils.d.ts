ced0fe42d1742f35cb347894c3bd051b9d7c81d5
asmit-patil <patilasmit0gmail.com>
2019-02-01 18:50:09 +0530

    added jsDoc and README.md file

--- typings/core/utils.d.ts
+++ typings/core/utils.d.ts
@@ -1,3 +1,3 @@
 export declare const mergeDeep: (target: any, source: any) => any;
-export declare const difference: (object: any, base: any) => any;
+export declare const difference: (object: any, base: any) => {}[];
 export declare const checkCyclic: (uid: any, mapping: any) => boolean;
