c098e49e7a178e37e9ac7fc42b19fd55644181bc
asmit-patil <patilasmit0gmail.com>
2019-02-03 18:44:49 +0530

    updated code of includeReference to solve the cyclic reference issue

--- src/core/utils.ts
+++ src/core/utils.ts
@@ -1,31 +1,33 @@
 import {isEqual, isObject, transform, uniq} from 'lodash'
 
-export const mergeDeep = (target, source) => {
+export const mergeDeep = (targett, sourcee) => {
     const self = this
-    const _merge_recursive = function(target, source) {
+    const mergeRrcursive = (target, source) => {
         for (const key in source) {
-            if (self._type(source[key]) == 'object' && self._type(target[key]) == self._type(source[key])) {
-                _merge_recursive(target[key], source[key])
-            } else if (self._type(source[key]) == 'array' && self._type(target[key]) == self._type(source[key])) {
+            if (self._type(source[key]) === 'object' && self._type(target[key]) === self._type(source[key])) {
+                mergeRrcursive(target[key], source[key])
+            } else if (self._type(source[key]) === 'array' && self._type(target[key]) === self._type(source[key])) {
                 target[key] = target[key].concat(source[key])
             } else {
                 target[key] = source[key]
             }
         }
     }
-    _merge_recursive(target, source)
-    return target
+    mergeRrcursive(targett, sourcee)
+
+    return targett
 }
 
-export const difference = (object, base) => {
-    function changes(object, base) {
-        return transform(object, function(result, value, key) {
+export const difference = (obj, basee) => {
+    const changes = (object, base) => {
+        return transform(object, (result, value, key) => {
             if (!isEqual(value, base[key])) {
                 result[key] = (isObject(value) && isObject(base[key])) ? changes(value, base[key]) : value
             }
         })
     }
-    return changes(object, base)
+
+    return changes(obj, basee)
 }
 
 export const checkCyclic = (uid, mapping) => {
