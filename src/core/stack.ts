bcecaadea47103edce4b9ab11caab677c602b590
asmit-patil <patilasmit0gmail.com>
2019-02-12 11:32:47 +0530

    added test cases

--- src/core/stack.ts
+++ src/core/stack.ts
@@ -35,7 +35,8 @@ export class Stack {
                 } else if (!this.config.hasOwnProperty('locales') || this.config.locales.length === 0) {
                     throw new Error('Please provide locales with code and relative_url_prefix.')
                 } else if (!(fs.existsSync(this.config.contentStore.baseDir))) {
-                    throw new Error(`${this.config.contentStore.baseDir} didn't exits.`)
+                    console.log(fs.existsSync(this.config.contentStore.baseDir), path.resolve(this.config.contentStore.baseDir))
+                    throw new Error(`${this.config.contentStore.baseDir} didn't exists.`)
                 } else {
                     this.baseDir = this.config.contentStore.baseDir
                     this.masterLocale = this.config.locales[0].code
