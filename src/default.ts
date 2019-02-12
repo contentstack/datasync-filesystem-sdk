d28a4a63138b44a9110d544bf5758361ed2a9075
asmit-patil <patilasmit0gmail.com>
2019-02-12 15:58:39 +0530

    updated src files structure and test cases

---
+++ src/default.ts
@@ -0,0 +1,12 @@
+/*!
+ * contentstack-sync-filsystem-sdk
+ * copyright (c) Contentstack LLC
+ * MIT Licensed
+ */
+
+export const defaultConfig = {
+  contentStore: {
+      baseDir: './_contents',
+    },
+  }
+
--- src/index.ts
+++
@@ -1,8 +0,0 @@
-/*!
- * contentstack-sync-filsystem-sdk
- * copyright (c) Contentstack LLC
- * MIT Licensed
- */
-import { Contentstack as contentstack } from './core/contentstack'
-const Contentstack = new contentstack()
-module.exports = Contentstack
