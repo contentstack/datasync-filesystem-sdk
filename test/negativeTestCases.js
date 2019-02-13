0e2d236f682cf17e756b22ab98156975529a8bb5
asmit-patil <patilasmit0gmail.com>
2019-02-13 22:27:50 +0530

    updated test cases with negative test cases

---
+++ test/negativeTestCases.js
@@ -0,0 +1,251 @@
+const Contentstack = require('../dist/contentstack').Contentstack
+
+let Stack = Contentstack.Stack({
+    api_key: '',
+    access_token: '',
+    'contentStore': {
+        'baseDir': './test/testData'
+    },
+    locales: [
+        {
+            code: 'en-us',
+            relative_url_prefix: '/'
+        },
+        {
+            code: 'es-es',
+            relative_url_prefix: '/es/'
+        }
+    ]
+
+})
+
+describe('negavtive test cases',()=>{
+
+    beforeEach(() => {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const value = await Stack.connect();
+                return resolve(value);
+            }
+            catch (reason) {
+                return reject(reason);
+            }
+        })
+        
+    })
+
+    test('call contentType() without uid', () => {
+       
+        expect(() => {
+            return Stack
+            .contentType()
+            .entries()
+            .find()
+          }).toThrow();
+    })
+
+    test('call entries() directly', () => {
+        
+        expect(() => {
+            return Stack
+            .entries()
+            .find()
+          }).toThrow();
+
+    })
+
+    test('call entry() directly', () => {
+        expect(() => {
+            return Stack
+            .entry()
+            .find()
+          }).toThrow()
+
+    })
+
+    test('getQuery()', () => {
+        expect(
+            Stack.contentType('product')
+            .entry()
+            .lessThan('created_at','2017-07-20')
+            .getQuery()
+          ).toEqual({ 'created_at': { '$lt': '2017-07-20' } });
+
+    })
+
+    test('lessThan()', () => {
+        expect(() => {
+            return Stack.contentType('product')
+            .entry()
+            .lessThan('')
+            .find()
+          }).toThrow()
+    })
+
+    test('containedIn()', () => {
+        expect(() => {
+            return Stack.contentType('product')
+            .entry()
+            .containedIn('')
+            .find()
+          }).toThrow()
+    })
+
+    test('exists()', () => {
+        expect(() => {
+            return Stack.contentType('product')
+            .entry()
+            .exists('')
+            .find()
+          }).toThrow()
+    })
+
+    test('ascending()', () => {
+        expect(() => {
+            return Stack.contentType('product')
+            .entry()
+            .ascending('')
+            .find()
+          }).toThrow()
+    })
+
+    test('skip()', () => {
+        expect(() => {
+            return Stack.contentType('product')
+            .entry()
+            .skip('')
+            .find()
+          }).toThrow()
+    })
+
+    test('equalTo()', () => {
+        expect(() => {
+            return Stack.contentType('product')
+            .entry()
+            .equalTo('')
+            .find()
+          }).toThrow()
+    })
+
+    test('language()', () => {
+        expect(() => {
+            return Stack
+            .asset('abc')
+            .language()
+            .find()
+          }).toThrow()
+    })
+
+    test('where()', () => {
+        expect(() => {
+            return Stack.contentType('product')
+            .entry()
+            .where('')
+            .find()
+          }).toThrow()
+    })
+
+    test('tags()', () => {
+        expect(() => {
+            return Stack.contentType('product')
+            .entry()
+            .tags('')
+            .find()
+          }).toThrow()
+    })
+
+    test('language()', () => {
+        expect(() => {
+            return Stack.contentType('product')
+            .entry()
+            .language('')
+            .find()
+          }).toThrow()
+    })
+
+    test('query()', () => {
+        expect(() => {
+            return Stack.contentType('product')
+            .entry()
+            .query('')
+            .find()
+          }).toThrow()
+    })
+
+    test('regex()', () => {
+        expect(() => {
+            return Stack.contentType('product')
+            .entry()
+            .regex('')
+            .find()
+          }).toThrow()
+    })
+
+    test('only()', () => {
+        expect(() => {
+            return Stack.contentType('product')
+            .entry()
+            .only('')
+            .find()
+          }).toThrow()
+    })
+    
+
+    test('except()', () => {
+        expect(() => {
+            return Stack.contentType('product')
+            .entry()
+            .except('')
+            .find()
+          }).toThrow()
+    })
+
+    test('queryReferences()', () => {
+        expect(() => {
+            return Stack.contentType('product')
+            .entry()
+            .queryReferences('')
+            .find()
+          }).toThrow()
+    })
+    
+    
+    test('get all entries from contentType of product', () => {
+        
+        return Stack.contentType('test')
+            .entry('abc')
+            .find()
+            .then(function (result) {
+            }).catch((error) => {
+                expect(error).toBe(error)
+            })
+    })
+
+    test('get all entries from contentType of product', () => {
+        return Stack.asset('acb')
+            .language('fr-fs')
+            .find()
+            .then(function (result) {
+            }).catch((error) => {
+                expect(error).toBe(error)
+            })
+    })
+
+
+
+    test('locale key', () => {
+        const cs = require('../dist/contentstack').Contentstack
+        //let cs = new cms()
+        let stack = cs.Stack({
+            'contentStore': {
+                'baseDir': './test/testData'
+            },
+            'locales':[]
+        })
+            return stack
+            .connect().then().catch((error)=>{
+                expect(error).toBe(error)
+            })
+    })
+
+})
