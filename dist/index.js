7776c393a9bf5fdc9af1f5b480749c790e7f37cb
asmit-patil <patilasmit0gmail.com>
2019-03-01 17:15:10 +0530

    baseDir bug fix and added optimization for includeReferences

--- dist/index.js
+++ dist/index.js
@@ -4,31 +4,11 @@
  * copyright (c) Contentstack LLC
  * MIT Licensed
  */
-
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
 Object.defineProperty(exports, "__esModule", { value: true });
-var stack_1 = require("./stack");
-
-var Contentstack = function () {
-    function Contentstack() {
-        _classCallCheck(this, Contentstack);
+const stack_1 = require("./stack");
+class Contentstack {
+    static Stack(...stackArguments) {
+        return new stack_1.Stack(...stackArguments);
     }
-
-    _createClass(Contentstack, null, [{
-        key: "Stack",
-        value: function Stack() {
-            for (var _len = arguments.length, stackArguments = Array(_len), _key = 0; _key < _len; _key++) {
-                stackArguments[_key] = arguments[_key];
-            }
-
-            return new (Function.prototype.bind.apply(stack_1.Stack, [null].concat(stackArguments)))();
-        }
-    }]);
-
-    return Contentstack;
-}();
-
+}
 exports.Contentstack = Contentstack;
--- dist/utils.js
+++ dist/utils.js
@@ -4,57 +4,34 @@
  * copyright (c) Contentstack LLC
  * MIT Licensed
  */
-
 Object.defineProperty(exports, "__esModule", { value: true });
-var lodash_1 = require("lodash");
-exports.difference = function (obj, baseObj) {
-    var changes = function changes(object, base) {
-        return lodash_1.transform(object, function (result, value, key) {
+const lodash_1 = require("lodash");
+exports.difference = (obj, baseObj) => {
+    const changes = (object, base) => {
+        return lodash_1.transform(object, (result, value, key) => {
             if (!lodash_1.isEqual(value, base[key])) {
-                result[key] = lodash_1.isObject(value) && lodash_1.isObject(base[key]) ? changes(value, base[key]) : value;
+                result[key] = (lodash_1.isObject(value) && lodash_1.isObject(base[key])) ? changes(value, base[key]) : value;
             }
         });
     };
     return changes(obj, baseObj);
 };
-exports.checkCyclic = function (uid, mapping) {
-    var flag = false;
-    var list = [uid];
-    var _iteratorNormalCompletion = true;
-    var _didIteratorError = false;
-    var _iteratorError = undefined;
-
-    try {
-        for (var _iterator = list[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-            var i = _step.value;
-
-            var parent = getParents(i, mapping);
-            if (parent.indexOf(uid) !== -1) {
-                flag = true;
-                break;
-            }
-            list = lodash_1.uniq(list.concat(parent));
+exports.checkCyclic = (uid, mapping) => {
+    let flag = false;
+    let list = [uid];
+    for (const i of list) {
+        const parent = getParents(i, mapping);
+        if (parent.indexOf(uid) !== -1) {
+            flag = true;
+            break;
         }
-    } catch (err) {
-        _didIteratorError = true;
-        _iteratorError = err;
-    } finally {
-        try {
-            if (!_iteratorNormalCompletion && _iterator.return) {
-                _iterator.return();
-            }
-        } finally {
-            if (_didIteratorError) {
-                throw _iteratorError;
-            }
-        }
+        list = lodash_1.uniq(list.concat(parent));
     }
-
     return flag;
 };
-var getParents = function getParents(child, mapping) {
-    var parents = [];
-    for (var key in mapping) {
+const getParents = (child, mapping) => {
+    const parents = [];
+    for (const key in mapping) {
         if (mapping[key].indexOf(child) !== -1) {
             parents.push(key);
         }
