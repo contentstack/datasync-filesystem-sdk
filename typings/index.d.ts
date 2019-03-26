fe636b611abca68863b1b430a38f65a74aff2283
asmit-patil <patilasmit0gmail.com>
2019-03-26 16:29:37 +0530

    added jsDoc document

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -4,6 +4,24 @@
  * MIT Licensed
  */
 import { Stack } from './stack';
+/**
+ * @method Contentstack
+ * @description Creates an instance of `Contentstack`.
+ * @api public
+ */
 export declare class Contentstack {
+    /**
+     * @method Stack
+     * @description Initialize an instance of ‘Stack’
+     * @api public
+     * @example
+     * const Stack = Contentstack.Stack('api_key', 'delivery_token')//or
+     * const Stack = Contentstack.Stack({
+     *  api_key: 'api_key',
+     *  token: 'delivery_token'
+     * })
+     *
+     * @returns {Stack}
+     */
     static Stack(...stackArguments: any[]): Stack;
 }
