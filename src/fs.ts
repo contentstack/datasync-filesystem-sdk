1dbc04c023903de0a526359a9108150fa1461bde
iyerrama29 <iyerrama29+contentstack.github@gmail.com>
2019-07-01 23:43:09 +0530

    v1.0.0 update. Step 2

--- src/fs.ts
+++ src/fs.ts
@@ -1,5 +1,17 @@
-import { existsSync, readFile as rf } from 'fs'
-import { promisify } from 'util'
+/*!
+ * Contentstack DataSync Filesystem SDK.
+ * Enables querying on contents saved via @contentstack/datasync-content-store-filesystem
+ * Copyright (c) Contentstack LLC
+ * MIT Licensed
+ */
+
+import {
+  existsSync,
+  readFile as rf,
+} from 'fs'
+import {
+  promisify,
+} from 'util'
 
 const promisifiedReadFile = promisify(rf)
 
@@ -13,4 +25,6 @@ export const readFile = async (path: string, type: string = 'utf-8') => {
   return []
 }
 
-export { existsSync } from 'fs'
+export {
+  existsSync,
+} from 'fs'
